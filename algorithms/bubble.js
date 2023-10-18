7c81f2a50cd579ca99bc95695838af478c573fce
anirudh716 <anirudhpandey244@gmail.com>
2023-10-19 01:39:46 +0530

    Added Bubble Sort Algorithm

---
+++ algorithms/bubble.js
@@ -0,0 +1,50 @@
+function bubbleSortVisualization() {
+    async function bubbleSort() {
+        var bars = document.querySelectorAll('.bar');
+        var n = bars.length;
+        var swapped = true;
+        while(swapped){
+            swapped = false;
+            for (var i = 0; i < n - 1; i++) {
+                bars[i].style.background = 'red';
+                bars[i + 1].style.background = 'red';
+
+                if (parseInt(bars[i].style.height) > parseInt(bars[i + 1].style.height)) {
+                    await sleep(10);
+                    swap(bars[i], bars[i + 1]);
+                    swapped = true;
+                }
+
+                bars[i].style.background = '#e1ad01';
+                bars[i + 1].style.background = '#e1ad01';
+            }
+            if(swapped){
+                bars[n - 1].style.background = 'green';
+                n--;
+            }
+        }
+        for (var i = 0; i < n; i++) {
+            if(window.getComputedStyle(bars[i]).backgroundColor != 'green')
+            bars[i].style.background = 'green';
+        }
+    }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function swap(el1, el2) {
+        const transform1 = el1.style.height;
+        const transform2 = el2.style.height;
+        el1.style.height = transform2;
+        el2.style.height = transform1;
+    }
+    var sortBtn = document.getElementById('sort-btn');
+    sortBtn.addEventListener('click', function () {
+        let selectedAlgo = document.querySelector('select').value;
+        if(selectedAlgo == 'bubble')
+            bubbleSort();
+    });
+}
+
+window.onload = bubbleSortVisualization;
--- algorithms/main.js
+++ algorithms/main.js
@@ -1,19 +1,18 @@
-const barsHeight = [];
+var barsHeight = [];
+var height = 4;
+var numberOfBars = 60;
 
-for(var i=0; i<100; i++){
-    var randomNumbers = Math.floor(Math.random()*101);
-    barsHeight.push(randomNumbers);
-}
 
 function createBars(){
     var barsContainer = document.getElementById('bars-container');
     barsContainer.innerHTML = '';
 
-    for(var i=0; i<100; i++){
+    for(var i=1; i<numberOfBars; i++){
         var barHeight = barsHeight[i];
         var bar = document.createElement('div');
         bar.className = 'bar';
-        bar.style.height = barHeight + 'px';
+        bar.style.height = barHeight * height + 'px';
+        if(barHeight == 0)  bar.style.height = '2px';
         barsContainer.appendChild(bar);
     }
 }
@@ -33,3 +32,4 @@ function generateRandomHeights(){
 }
 generateRandomHeights();
 createBars();
+
